cdb56d5c6da0e3362c3f670edf2256dee7fcee63
Flaviane Ribeiro <rnany13@gmail.com>
2018-07-15 17:52:51 +0000

    alteração

--- index.php
+++ index.php
@@ -7,13 +7,14 @@
         <title>Tutorial</title>
         
         <script src="https://code.jquery.com/jquery-3.3.1.min.js" type="text/javascript"></script>
-        <script type="text/javascript" src="script.js"></script>
+        <script src="script.js" type="text/javascript"></script>
     </head>
     <body onload="carregarLivros()">
         <div class="container">
             <div class="row">
                 <div class="col-md-12">
-                    <h2>Lista de Livro</h2>
+                    <h1>Lista de Livro</h1>
+                    <h2></h2>
                     <table id="minhaTabela" class="table table-bordered">
                         <thead>
                             <tr>
--- script.js
+++ script.js
@@ -1,36 +1,39 @@
 function carregarLivros(){
-    var Itens = "", url = "dados/dados.php";
-    
-    //Carregar dados
+	//variáveis
+	var itens = "", url = "dados/dados.php";
+
+    //Capturar Dados Usando Método AJAX do jQuery
     $.ajax({
-        url: url,
-        cache:false,
-        dataType: "json",
-        beforeSend: function(){
-            $("h2").html("Carregando ...");
-        },
-        error: function() {
-            $("h2").html("Ha algum problema de dados");
-        },
-        sucess: function(retorno) {
-            if(retorno[0].erro){
-                $("h2").html(retorno);
-            }else {
-                for(var i = 0; i < retorno.length; i++ ){
-                    itens += "<tr>";
-                    itens += "<td> " + retorno[i].codigo + "</td>";
-                    itens += "<td> " + retorno[i].nome + "</td>";
-                    itens += "<td> " + retorno[i].autor + "</td>";
-                    itens += "<td> " + retorno[i].valor + "</td>";
-                    itens += "<td> " + retorno[i].quantidade + "</td>";
-                    itens += "</tr>";
-                }
-                
-                //preencer
-                $("#minhaTabela tbody").html(itens);
-                
-                $("h2").html("Carregando");
-            }
-        }
+	    url: url,
+	    cache: false,
+	    dataType: "json",
+	    beforeSend: function() {
+		    $("h2").html("Carregando..."); //Carregando
+	    },
+	    error: function() {
+		    $("h2").html("Ha algum problema com a fonte de dados");
+	    },
+	    success: function(retorno) {
+		    if(retorno[0].erro){
+			    $("h2").html(retorno[0].erro);
+		    }
+		    else{
+			    //Laço para criar linhas da tabela
+			    for(var i = 0; i<retorno.length; i++){
+				    itens += "<tr>";
+				    itens += "<td>" + retorno[i].codigo + "</td>";
+				    itens += "<td>" + retorno[i].nome + "</td>";
+				    itens += "<td>" + retorno[i].autor + "</td>";
+				    itens += "<td>" + retorno[i].valor + "</td>";
+				    itens += "<td>" + retorno[i].quantidade + "</td>";
+				    itens += "</tr>";
+			    }
+			    //Preencher a Tabela
+			    $("#minhaTabela tbody").html(itens);
+			    
+			    //Limpar Status de Carregando
+			    $("h2").html("Carregado");
+		    }
+	    }
     });
 }
